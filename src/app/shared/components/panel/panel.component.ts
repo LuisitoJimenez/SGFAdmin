675bc55f0f491b466ac6beaa8be200e8118dab80
Luis Jimenez <wichin@outlook.com>
2023-12-15 19:38:35 -0600

    Refactor code to improve performance and readability

--- src/app/shared/components/panel/panel.component.ts
+++ src/app/shared/components/panel/panel.component.ts
@@ -1,9 +1,24 @@
 import { DataService } from './../../../services/data.service';
-import { ChangeDetectorRef, Component, Inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ChangeDetectorRef, Component, ElementRef, Inject, ViewChild, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatSelectChange } from '@angular/material/select';
+import { Observable, map, startWith } from 'rxjs';
 import { DefinitionAccordion } from 'src/app/models/definition';
+
+//test
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import { MatChipInputEvent } from '@angular/material/chips';
+
+//test autocomplete
+export interface State {
+  flag: string;
+  name: string;
+  population: string;
+}
+
 
 @Component({
   selector: 'app-panel',
@@ -12,6 +27,7 @@ import { DefinitionAccordion } from 'src/app/models/definition';
 })
 export class PanelComponent {
 
+  appliedFilters: { [key: string]: any } = {};
   openStep = 0;
   uploadElementName: any;
   initialDataClone: any;
@@ -35,6 +51,7 @@ export class PanelComponent {
   selectedOption: any;
   optionsList: any[] = [];
   originalImageUrl: string = '';
+  originalOptions: { [key: string]: any[] } = {};
   selectedLabel: string = '';
   linkedElements: { [key: string]: any[] } = {};
   linkedElement: string[] = [];
@@ -46,7 +63,6 @@ export class PanelComponent {
     private dialogRef: MatDialogRef<PanelComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private cd: ChangeDetectorRef,
-
     private dataService: DataService
   ) {
     this.openStep = data.openStep;
@@ -54,6 +70,18 @@ export class PanelComponent {
     this.userId = data.userId;
     this.operation = data.operation;
     this.itemData = data.data;
+
+    //test
+    this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
+      startWith(null),
+      map((fruit: string | null) => (fruit ? this._filter(fruit) : this.allFruits.slice())),
+    );
+
+    //test autocomplete
+    this.filteredStates = this.stateCtrl.valueChanges.pipe(
+      startWith(''),
+      map(state => (state ? this._filterStates(state) : this.states.slice())),
+    );
   }
 
   setStep(index: number) {
@@ -77,8 +105,6 @@ export class PanelComponent {
     this.initialDataClone = JSON.parse(JSON.stringify(this.initialData));
   }
 
-  originalOptions: { [key: string]: any[] } = {};
-
   onSelectionChange(event: any, element: any) {
     const selectedOption = event.options[0].value;
     const selectedLabelWithNumber = this.filteredOptions[element].find((option: any) => option.value === selectedOption).label;
@@ -88,12 +114,10 @@ export class PanelComponent {
     if (event.options[0].selected) {
       if (element === 'titularPlayers') {
         this.filteredOptions['substitutePlayers'] = this.filteredOptions['substitutePlayers'].filter((option: any) => option.value !== selectedOption);
-      }
-      else if (element === 'substitutePlayers') {
+      } else if (element === 'substitutePlayers') {
         this.filteredOptions['titularPlayers'] = this.filteredOptions['titularPlayers'].filter((option: any) => option.value !== selectedOption);
       }
-    }
-    else {
+    } else {
       this.filteredOptions['titularPlayers'] = [...this.originalOptions['titularPlayers']];
       this.filteredOptions['substitutePlayers'] = [...this.originalOptions['substitutePlayers']];
     }
@@ -107,95 +131,17 @@ export class PanelComponent {
     });
   }
 
-/*   onSelectionChange(event: any, element: any) {
-    const selectedOption = event.options[0].value;
-    const selectedLabelWithNumber = this.filteredOptions[element].find((option: any) => option.value === selectedOption).label;
-    this.selectedLabel = selectedLabelWithNumber.split(' ').slice(1).join(' ');
-    this.form.get(this.linkedElement[element])?.setValue(this.selectedLabel);
-    const isSelected = event.options.some((option: any) => option.value === selectedOption);
-    if (event.options[0].selected) {
-      if (element === 'titularPlayers') {
-        this.filteredOptions['substitutePlayers'] = this.filteredOptions['substitutePlayers'].filter((option: any) => option.value !== selectedOption);
-      }
-      else if (element === 'substitutePlayers') {
-        this.filteredOptions['titularPlayers'] = this.filteredOptions['titularPlayers'].filter((option: any) => option.value !== selectedOption);
-      }
-    }
-    else {
-      if (element === 'titularPlayers') {
-        this.filteredOptions['substitutePlayers'] = this.originalOptions['substitutePlayers'].filter((option: any) => {
-          return this.form.get('substitutePlayers')?.value?.includes(option.value) || !this.form.get('titularPlayers')?.value?.includes(option.value);
-        });
-      } else if (element === 'substitutePlayers') {
-        this.filteredOptions['titularPlayers'] = this.originalOptions['titularPlayers'].filter((option: any) => {
-          return this.form.get('titularPlayers')?.value?.includes(option.value) || !this.form.get('substitutePlayers')?.value?.includes(option.value);
-        });
-      }
-    }
-
-    // Aplica los filtros de los select
-    Object.keys(this.appliedFilters).forEach(filterField => {
-      this.filteredOptions[element] = this.filteredOptions[element].filter((option: any) => {
-        return option[filterField] === this.appliedFilters[filterField];
-      });
-    });
-  } */
-
-/*   onSelectionChange(event: any, element: any) {
-    const selectedOption = event.options[0].value;
-    const selectedLabelWithNumber = this.filteredOptions[element].find((option: any) => option.value === selectedOption).label;
-    this.selectedLabel = selectedLabelWithNumber.split(' ').slice(1).join(' ');
-    this.form.get(this.linkedElement[element])?.setValue(this.selectedLabel);
-    const isSelected = event.options.some((option: any) => option.value === selectedOption);
-    if (event.options[0].selected) {
-      if (element === 'titularPlayers') {
-        this.filteredOptions['substitutePlayers'] = this.filteredOptions['substitutePlayers'].filter((option: any) => option.value !== selectedOption);
-      }
-      else if (element === 'substitutePlayers') {
-        this.filteredOptions['titularPlayers'] = this.filteredOptions['titularPlayers'].filter((option: any) => option.value !== selectedOption);
-      }
-    }
-    else {
-      if (element === 'titularPlayers') {
-        this.filteredOptions['substitutePlayers'] = this.originalOptions['substitutePlayers'].filter((option: any) => {
-          return this.form.get('substitutePlayers')?.value?.includes(option.value) || !this.form.get('titularPlayers')?.value?.includes(option.value);
-        });
-      } else if (element === 'substitutePlayers') {
-        this.filteredOptions['titularPlayers'] = this.originalOptions['titularPlayers'].filter((option: any) => {
-          return this.form.get('titularPlayers')?.value?.includes(option.value) || !this.form.get('substitutePlayers')?.value?.includes(option.value);
-        });
-      }
-    }
-  } */
-
   applyFilter(event: Event, element: any) {
     const filterWords = this.removeAccents((event.target as HTMLInputElement).value.trim().toLowerCase()).split(' ');
-
     this.filteredOptions[element] = this.options[element].filter((option: any) => {
-      // Aplica el filtro de búsqueda
       const optionWords = this.removeAccents(option.label.toLowerCase()).split(' ');
       const matchesSearchFilter = filterWords.every(filterWord => optionWords.some(optionWord => optionWord.startsWith(filterWord)));
-
-      // Aplica los filtros de los select
       const matchesSelectFilters = Object.keys(this.appliedFilters).every(filterField => {
         return option[filterField] === this.appliedFilters[filterField];
       });
-
       return matchesSearchFilter && matchesSelectFilters;
     });
   }
-
-/*   applyFilter(event: Event, element: any) {
-    console.log(event);
-    console.log(element);
-    const filterWords = this.removeAccents((event.target as HTMLInputElement).value.trim().toLowerCase()).split(' ');
-    this.filteredOptions[element] = this.options[element].filter((option: any) => {
-      const optionWords = this.removeAccents(option.label.toLowerCase()).split(' ');
-      return filterWords.every(filterWord => optionWords.some(optionWord => optionWord.startsWith(filterWord)));
-    });
-  } */
-
-  appliedFilters: { [key: string]: any } = {};
 
   applyFilter2(event: MatSelectChange, filterField: string) {
     const filterValue = event.value;
@@ -211,20 +157,7 @@ export class PanelComponent {
       });
     });
   }
-
-/*   applyFilter2(event: MatSelectChange, filterField: string) {
-    const filterValue = event.value;
-    Object.keys(this.linkedElements).forEach(element => {
-      const linkedElementNames = this.linkedElements[element];
-      linkedElementNames.forEach((linkedElementName: any) => {
-        this.filteredOptions[linkedElementName] = this.options[linkedElementName].filter((option: any) => {
-          return option[filterField] === filterValue;
-        });
-        console.log(this.filteredOptions[linkedElementName]);
-      });
-    });
-  } */
-
+  
   removeAccents(str: string) {
     return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
   }
@@ -322,6 +255,95 @@ export class PanelComponent {
     }
     this.panelIcon = hasRequiredFields ? ['check_circle'] : ['info'];
     return true;
+  }
+
+
+  //test
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  fruitCtrl = new FormControl('');
+  filteredFruits: Observable<string[]>;
+  fruits: string[] = ['Lemon'];
+  allFruits: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
+
+  @ViewChild('fruitInput') fruitInput!: ElementRef<HTMLInputElement>;
+
+  announcer = inject(LiveAnnouncer);
+
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.fruits.push(value);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+
+    this.fruitCtrl.setValue(null);
+  }
+
+  remove(fruit: string): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+
+      this.announcer.announce(`Removed ${fruit}`);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    console.log(event);
+    console.log(event.option.viewValue);
+    this.fruits.push(event.option.viewValue);
+    this.fruitInput.nativeElement.value = '';
+    this.fruitCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.allFruits.filter(fruit => fruit.toLowerCase().includes(filterValue));
+  }
+
+  //test autocomplete
+  stateCtrl = new FormControl('');
+  filteredStates: Observable<State[]>;
+
+  states: State[] = [
+    {
+      name: 'Arkansas',
+      population: '2.978M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg',
+    },
+    {
+      name: 'California',
+      population: '39.14M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_California.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg',
+    },
+    {
+      name: 'Florida',
+      population: '20.27M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Florida.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg',
+    },
+    {
+      name: 'Texas',
+      population: '27.47M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Texas.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg',
+    },
+  ];
+
+
+
+  private _filterStates(value: string): State[] {
+    const filterValue = value.toLowerCase();
+
+    return this.states.filter(state => state.name.toLowerCase().includes(filterValue));
   }
 
 }
