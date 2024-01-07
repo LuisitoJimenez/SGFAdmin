9b7eb31b53d2f3902e671bf3129e90ef78751346
Luis Jimenez <wichin@outlook.com>
2024-01-07 02:20:01 -0600

    Update teamFormDefinition to show ID field

--- src/app/modules/teams/team-definition.ts
+++ src/app/modules/teams/team-definition.ts
@@ -40,7 +40,7 @@ export const teamFormDefinition: DefinitionAccordion[] = [
         name: 'id',
         label: 'ID',
         element: FormElement.input,
-        hidden: true,
+        hidden: false,
         colClass: 'col-sm-3',
         disabled: true,
         readonly: true,
